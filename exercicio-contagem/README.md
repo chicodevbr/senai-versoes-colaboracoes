85d8f7f7bb53530e716e313d3ccb061ea7afdd66
F. Júnior <chicodevbr@gmail.com>
2021-08-28 21:41:12 -0300

    resolvendo exercício de lógica do encontro remoto 2

---
+++ exercicio-contagem/README.md
@@ -0,0 +1,3 @@
+# Resolução de Exercício
+
+Contagem de alunos em lista para saber quais números são pares e ímpares.
--- exercicio-contagem/app.js
+++ exercicio-contagem/app.js
@@ -1,8 +1,10 @@
-let numeroAlunos = 10;
+let numeroAlunos = 30;
 
 for (let i = 0; i <= numeroAlunos; i++) {
   if (i === 0) {
     console.log("O número atual é zero");
+  } else if (i % 2 === 0 && i > 5) {
+    console.log(`${i} é par e maior do que 5`);
   } else if (i % 2 === 0) {
     console.log(`${i} é par`);
   } else {
@@ -10,14 +12,18 @@ for (let i = 0; i <= numeroAlunos; i++) {
   }
 }
 
-let listaAlunos = ["Marcelo", "Joana", "Cleber", "Sebastião", "Tostes"];
+let listaAlunos = ["Joana", "Bia", "Ana", "Zé", "Pedro", "Jeff", "Oli"];
 
-/* for (const aluno of listaAlunos) {
+for (const aluno of listaAlunos) {
   console.log(aluno);
-} */
+}
 
-/*
-listaAlunos.forEach((element) => {
-  console.log(element);
-});
-*/
+for (let aluno = 0; aluno <= listaAlunos.length; aluno++) {
+  if (aluno === 0) {
+    console.log("o número é zero");
+  } else if (aluno % 2 === 0) {
+    console.log(`${aluno} é par`);
+  } else {
+    console.log(`${aluno} é ímpar`);
+  }
+}
